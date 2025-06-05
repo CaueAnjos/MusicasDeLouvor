ec1e2af552986da312f625839d3aee05bf8b7b07
CaueAnjos <kawidosanjos@outlook.com>
2025-06-04 21:06:09 -0300

    add StringUtils

--- LouvorHelper/Utils.cs
+++ LouvorHelper/Utils.cs
@@ -6,25 +6,6 @@ namespace LouvorHelper;
 
 class Utils
 {
-    public static string RemoveDiacritics(string text)
-    {
-        var normalizedString = text.Normalize(NormalizationForm.FormD);
-        var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
-
-        for (int i = 0; i < normalizedString.Length; i++)
-        {
-            char c = normalizedString[i];
-            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-            {
-                stringBuilder.Append(c);
-            }
-        }
-
-        return stringBuilder
-            .ToString()
-            .Normalize(NormalizationForm.FormC);
-    }
 
     public enum UrlStyle
     {
