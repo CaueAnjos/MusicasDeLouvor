000f87702ff74c4b1056b95ee4a79dfb780ac580
CaueAnjos <kawidosanjos@outlook.com>
2025-06-17 18:19:27 -0300

    refactor(utils): remove unused utility

--- LouvorHelperCore/utils/ProviderReturnPair.cs
+++
@@ -1,9 +0,0 @@
-namespace LouvorHelperCore.Utils;
-
-internal static class ProviderReturnPair
-{
-    public static KeyValuePair<string, string?> ReturnPair(string label, string? lyrics = null)
-    {
-        return new KeyValuePair<string, string?>(label, lyrics);
-    }
-}
